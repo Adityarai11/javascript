f74f7e46f2d3189e26cb8b3eecc71f3c1eb9996e
Aditya rai <raijiaditya9868@gmail.com>
2024-05-14 15:41:37 +0530

    basic project 4

---
+++ project 4/script.js
@@ -0,0 +1,27 @@
+const istatus = document.querySelector("h5")
+const btn = document.querySelector("#addfre")
+let check = 0;
+
+btn.addEventListener("click",function(){
+    if (check == 0 ) {
+    istatus.innerHTML= "Friends";
+    istatus.style.color = "green";
+    btn.innerHTML="Remove friend"
+    check = 1;
+    }else{
+        istatus.innerHTML= "Stringer";
+        istatus.style.color = "red";
+        btn.innerHTML="Add friend"
+        check = 0;
+    }
+
+
+})
+// const remove = document.querySelector("#remove")
+
+// remove.addEventListener("click",function(){
+//     istatus.innerHTML= "Stranger";
+//     istatus.style.color = "red";
+
+
+// })
